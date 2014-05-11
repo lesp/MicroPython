19d62882f16501be3f1ae6396acf7600c18a103e
Les Pounder <lespounder@gmail.com>
2014-05-11 22:12:15 +0100

    updated to include y axis

--- accel.py
+++ accel.py
@@ -1,18 +1,30 @@
 # main.py -- put your code here!
 import pyb
+from time import sleep
 accel = pyb.Accel()
-SENSITIVITY = 3
+SENSITIVITY = 6
 x1_pin = pyb.Pin.board.X1
 g = pyb.Pin(pyb.Pin.board.X1, pyb.Pin.OUT_PP)
+x12_pin = pyb.Pin.board.X12
+h = pyb.Pin(pyb.Pin.board.X12, pyb.Pin.OUT_PP)
 
 def lightup():
     g.high()
 
+def lightup2():
+    h.high()
+
 while True:
     x = accel.x()
+    y = accel.y()
     if abs(x) > SENSITIVITY: 
         lightup()
+        sleep(0.5)
+    elif abs(y) > SENSITIVITY:
+        lightup2()
+        sleep(0.5)
     else:
         g.low()
+        h.low()
 
     pyb.delay(100)
